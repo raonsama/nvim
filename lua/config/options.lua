be412f738d264adf77b21f78f7c574f7a33c17ac
Raymond M <raonsama@proton.me>
2024-02-13 04:47:12 +0800

    Update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,7 @@
 local opt = vim.opt
 opt.shiftwidth = 4 -- Size of an indent
 opt.tabstop = 4 -- Number of spaces tabs count for
+opt.softtabstop = 0 -- Number of columns for a TAB
 opt.wrap = true -- Disable line wrap
 opt.pumheight = 5 -- Maximum number of entries in a popup
--- opt.listchars = "eol:⏎,tab:␉‣,space:·" -- Invisible character
+opt.listchars = "tab:␉·" -- Invisible character
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,11 +5,36 @@ return {
     opts = function(_, opts)
       local indent = {
         function()
-          local style = vim.bo.expandtab and "Spaces" or "Tab Size"
-          local size = vim.bo.expandtab and vim.bo.tabstop or vim.bo.shiftwidth
-          return style .. ": " .. size
+          local space_pat = [[\v^ +]]
+          local tab_pat = [[\v^\t+]]
+          local space_indent = vim.fn.search(space_pat, 'nwc')
+          local tab_indent = vim.fn.search(tab_pat, 'nwc')
+
+          if space_indent > 0 and tab_indent == 0 then
+            return "Spaces: " .. space_indent
+          elseif tab_indent > 0 and space_indent == 0 then
+            return "Tabs: " .. tab_indent
+          elseif tab_indent == 0 and space_indent == 0 then
+            return ""
+          end
+
+          local mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+
+          if mixed_same_line > 0 then
+            return 'Mixed: ' .. mixed_same_line
+          end
+
+          local space_indent_cnt = vim.fn.searchcount({pattern = space_pat, max_count = 1e3}).total
+          local tab_indent_cnt = vim.fn.searchcount({pattern = tab_pat, max_count = 1e3}).total
+
+          if space_indent_cnt > tab_indent_cnt then
+            return 'Mixed: ' .. tab_indent
+          else
+            return 'Mixed: ' .. space_indent
+          end
         end
       }
+
       table.insert(opts.sections.lualine_x, indent)
       table.insert(opts.sections.lualine_x, "encoding")
       table.insert(opts.sections.lualine_x, "fileformat")
