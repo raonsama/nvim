7439213692aadeefe83331f091205d849551e1cf
Raymond M <raonsama@proton.me>
2024-02-15 00:45:34 +0800

    Move to options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.loaded_netrwPlugin = 0
+
 local opt = vim.opt
 -- opt.shiftwidth = 4 -- Size of an indent
 -- opt.tabstop = 4 -- Number of spaces tabs count for
