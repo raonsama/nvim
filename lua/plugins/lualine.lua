05f7898de034b5d1838ab141bad4f22a53875cfe
Raymond M <raonsama@proton.me>
2024-02-15 10:34:18 +0800

    Add indicator lsp

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -24,9 +24,30 @@ return {
         end
       }
 
+      local check_lsp = {
+        function()
+          local msg = 'No Active'
+          local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+          local clients = vim.lsp.get_active_clients()
+          if next(clients) == nil then
+            return msg
+          end
+          for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+              return client.name
+            end
+          end
+          return msg
+        end,
+        icon = ' LSP:',
+      }
+
       table.insert(opts.sections.lualine_x, indent)
-      table.insert(opts.sections.lualine_x, "encoding")
-      table.insert(opts.sections.lualine_x, "fileformat")
+      table.insert(opts.sections.lualine_x, { "encoding", fmt = string.upper })
+      table.insert(opts.sections.lualine_x, { "fileformat", icons_enabled = false, fmt = string.upper })
+
+      opts.sections.lualine_z = { check_lsp, }
     end
   }
 }
