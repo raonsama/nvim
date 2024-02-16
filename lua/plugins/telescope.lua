579c3787a28803f6575cacaaeec8df0bccd90685
Raymond M <raonsama@proton.me>
2024-02-16 16:19:00 +0800

    File and text search in hidden files and directories

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,18 @@
+local telescopeConfig = require("telescope.config")
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
 return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
         layout_strategy = "vertical",
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
       },
     },
   },
