ad8e4d59885c0209c05a9f254e53a25e358112b7
Raymond M <raonsama@proton.me>
2024-02-17 01:31:41 +0800

    Update multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,31 +1,15 @@
-return {
-  {
-    "smoka7/multicursors.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "smoka7/hydra.nvim",
-    },
-
-    opts = {
-      hint_config = false, -- { border = "rounded" },
-    },
-
-    cmd = { 
-      "MCstart", 
-      "MCvisual", 
-      "MCclear", 
-      "MCpattern", 
-      "MCvisualPattern", 
-      "MCunderCursor" 
-    },
-
-    keys = {
-      {
-        mode = { "v", "n" },
-        "<Leader>m",
-        "<cmd>MCvisual<cr>",
-        desc = "Create a multicursors",
-      },
-    },
-  },
+return { -- Multi Cursor
+  "mg979/vim-visual-multi",
+  event = "VeryLazy",
+  init = function()
+    vim.g.VM_theme = "purplegray"
+    vim.g.VM_mouse_mappings = 1
+    vim.g.VM_set_statusline = 0
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-d>",
+      ["Find Subword Under"] = "<C-d>",
+      ["Select Cursor Down"] = "<M-C-Down>",
+      ["Select Cursor Up"] = "<M-C-Up>",
+    }
+  end,
 }
