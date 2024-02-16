25df5f9caa74b4c59b08922d9ef0c0c5fcd83546
Raymond M <raonsama@proton.me>
2024-02-17 01:09:53 +0800

    Fullscreen lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,31 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+
+-- lazygit
+vim.keymap.set("n", "<leader>gg",
+  function()
+    Util.terminal({ "lazygit" }, {
+      cwd = Util.root(),
+      esc_esc = false,
+      ctrl_hjkl = false,
+      size = {
+        width = 1,
+        height = 1,
+      },
+    })
+  end, { desc = "Lazygit (root dir)" })
+
+vim.keymap.set("n", "<leader>gG",
+  function()
+    Util.terminal({ "lazygit" }, {
+      esc_esc = false,
+      ctrl_hjkl = false,
+      size = {
+        width = 1,
+        height = 1,
+      },
+    })
+  end, { desc = "Lazygit (cwd)" })
