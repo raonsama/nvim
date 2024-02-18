6ce39fd44613b3edfeb768e6a59d2a7a8f5ec045
Raymond M <raonsama@proton.me>
2024-02-18 20:13:23 +0800

    Remapping
    2nd attampt

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,9 +1,17 @@
 return { 
-  "mg979/vim-visual-multi",
-  event = "LazyFile",
-  init = function()
-    vim.g.VM_theme = "purplegray"
-    vim.g.VM_mouse_mappings = 1
-    vim.g.VM_set_statusline = 0
-  end,
+  {
+    "mg979/vim-visual-multi",
+    event = "LazyFile",
+    init = function()
+      vim.g.VM_theme = "purplegray"
+      vim.g.VM_mouse_mappings = 0
+      vim.g.VM_set_statusline = 0
+      vim.g.VM_maps = {
+        ["Move Left"] = "",
+        ["Move Right"] = "",
+        ["Select h"] = "",
+        ["Select l"] = "",
+      }
+    end
+  },
 }
