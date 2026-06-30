54a2022b8e6ec8657c5be4200a54dee5be2bda23
Raymond M <raonsama@proton.me>
2026-06-30 08:58:33 +0800

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.php" },
-    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.go" },
