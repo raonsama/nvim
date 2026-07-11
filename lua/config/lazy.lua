5b3501ce806b80a4f90d5fe3107707c3ae859832
Raymond M <raonsama@proton.me>
2026-07-11 13:44:23 +0800

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.php" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    --{ import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
